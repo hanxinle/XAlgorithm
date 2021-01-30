e8ed8941dcd3e751c25637feccded9deb6b7c457
hanxinle <hanxinle199@gmail.com>
2021-01-30 21:15:35 +0800

    2 clasic solutions

---
+++ c_cpp/classic/delete_white_space.cpp
@@ -0,0 +1,82 @@
+﻿/**
+  * 题目1：用熟悉的语言写一个函数，把字符串里面的空格全部去掉，
+  * 并返回删除的空格的个数，不允许开辟空间，只能申请简单类型的自动变量
+  * 要求时间复杂度 O(n)
+  * Time Complexity : O(n)
+  * Space Complexity : O(1)
+  */
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    static int DeleteWhiteSpace(string &s) {
+        int res = 0;
+
+        if (s.size() <= 0)
+            return 0;
+
+        auto st = s.begin();
+        auto cur = st;
+
+        while (cur != s.end()) {
+            if (isspace(*cur)) {
+                res++;
+                cur++;
+            }
+            else {
+                *st = *cur;
+                cur++;
+                st++;
+            }
+        }
+        s.erase(st, s.end());
+        return res;
+    }
+};
+
+void test(string & test_string) {
+    cout << "original input s=" << "\"" << test_string << "\"" << endl;
+    cout << "count of WhiteSpace(s) is " << Solution::DeleteWhiteSpace(test_string) << endl;
+    cout << "after delete white space(s) s=" << "\"" << test_string << "\"" << endl;
+}
+
+
+int main() {
+    string s1 = " a b c  ";
+    string s2 = " a b  c";
+    string s3 = "";
+    test(s1);
+    test(s2);
+    test(s3);
+
+    return 0;
+}
+
+//--------------------------------------------------------------------
+// 这个是我视频面试的时的解法，时间复杂度时O(n),使用一个辅助的字符串拼接
+// 去除空格的结果，空间复杂度是O(n).
+//class Solution {
+//public:
+//    static int DeleteWhiteSpace(string &s) {
+//        int res = 0;
+//        string ss = "";
+//        // 输入检查
+//        if (s.size() <= 0)
+//            return 0;
+//        // 主要功能
+//        for (auto it = s.begin(); it != s.end(); it++) {
+//            if (isspace(*it))
+//                res++;
+//            else
+//                ss += *it;
+//        }
+//        s = ss;
+//        cout << "In DelSpace s = " << s << endl;
+//        return res;
+//    }
+//
+//};
+//--------------------------------------------------------------------
---
+++ c_cpp/classic/query_from_cache.cpp
@@ -0,0 +1,128 @@
+﻿/**
+  * 题目2：已有1个库函数query，实现一个本地缓存函数 query_from_cache，
+  * n秒内调用相同的key，返回query上一次的内容。
+  * 这个题目和 leetcode 146 LRU Cache 类似
+  * Time Complexity: Cache(int): O(1)
+  *                  putInCache: O(1)
+  * (query_from_cache)QueryFromCache: O(1)
+  * Space Complexity: O(n)
+  */
+#include <iostream>
+#include <unordered_map>
+#include <list>
+#include <algorithm>
+#include <cassert>
+
+using namespace std;
+
+class Cache {
+private:
+    class Node {
+    public:
+        int key, val;
+        Node * prev, *next;
+        Node(int k, int v) :key(k), val(v), prev(nullptr), next(nullptr) {
+        }
+    };
+
+    int capacity;
+    unordered_map<int, Node*> search_in_cache;
+
+    Node * dummyHead;
+    Node * tail;
+
+public:
+    Cache(int cap = 3) :capacity(cap) {
+        dummyHead = new Node(-1, -1);
+        tail = new Node(-1, -1);
+
+        dummyHead->next = tail;
+        tail->prev = dummyHead;
+    }
+    // 释放双向链表
+    ~Cache() {
+        Node * cur = dummyHead;
+        while (cur != nullptr) {
+            Node * p = cur->next;
+            delete cur;
+            cur = nullptr;
+            cur = p;
+        }
+    }
+
+    void putInCache(int key, int value) {
+        if (search_in_cache.count(key)) {
+            search_in_cache[key]->val = value;
+            moveToHead(search_in_cache[key]);
+            return;
+        }
+
+        search_in_cache[key] = new Node(key, value);
+        addFirst(search_in_cache[key]);
+
+        if (search_in_cache.size() > capacity) {
+            int del_key = removeLast();
+            search_in_cache.erase(del_key);
+        }
+    }
+    // query_from_cache
+    int QueryFromCache(int key) {
+        if (search_in_cache.count(key)) {
+            moveToHead(search_in_cache[key]);
+            return dummyHead->next->val;
+        }
+
+        return -1;
+    }
+private:
+    // 链表中节点 node 移动到表头（dummyHead下一个）
+    void moveToHead(Node* node) {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+
+        node->prev = node->next = NULL;
+        addFirst(node);
+    }
+
+    // 添加节点 node 到表头
+    void addFirst(Node* node) {
+
+        node->next = dummyHead->next;
+        node->next->prev = node;
+
+        node->prev = dummyHead;
+        dummyHead->next = node;
+    }
+
+    // 移除尾部节点（非tail）
+    int removeLast() {
+        if (tail->prev != dummyHead) {
+            Node* del_node = tail->prev;
+
+            tail->prev = tail->prev->prev;
+            tail->prev->next = tail;
+            int res = del_node->key;
+
+            delete del_node;
+            del_node = nullptr;
+
+            return res;
+        }
+    }
+};
+
+int main() {
+    Cache cache(2);
+    cache.putInCache(1, 1);
+    cache.putInCache(2, 2);
+    cout << cache.QueryFromCache(1) << endl;  // 1
+    cache.putInCache(3, 3);
+    cout << cache.QueryFromCache(2) << endl;  //-1
+    cache.putInCache(4, 4);
+    cout << cache.QueryFromCache(1) << endl;  //-1
+    cout << cache.QueryFromCache(3) << endl;  // 3
+    cout << cache.QueryFromCache(4) << endl;  // 4
+
+    return 0;
+
+}
