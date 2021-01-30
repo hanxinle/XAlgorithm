ad9064b411734dfccc0866aecd90dfa7df7829ee
hanxinle <hanxinle199@gmail.com>
2021-01-30 21:48:38 +0800

    more cpp form changes

--- c_cpp/huawei/rm_取近似值.cpp
+++ c_cpp/huawei/rm_取近似值.cpp
@@ -1,14 +1,14 @@
-﻿//#include <iostream>
-//using namespace std;
-//
-//int main() {
-//    double num;
-//
-//    cin >> num;
-//
-//    if (num - int(num) >= 0.5)
-//        cout << (int)num + 1 << endl;
-//    else
-//        cout << (int)num;
-//    return 0;
-//}
+﻿// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     double num;
+
+//     cin >> num;
+
+//     if (num - static_cast<int>(num) >= 0.5)
+//         cout << static_cast<int>(num) + 1 << endl;
+//     else
+//         cout << static_cast<int>(num);
+//     return 0;
+// }
